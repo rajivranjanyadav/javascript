0bb486bb45db971a45dbc9fed54a34067361009b
rajivranjanyadav <160840661+rajivranjanyadav@users.noreply.github.com>
2024-03-16 14:16:21 +0000

    variable done

--- basic/class1.js
+++ basic/class1.js
@@ -1,1 +1,19 @@
-console.log("Rajiv Ranjan Yadav")
+// const accountId=1234552
+// let accountEmail="rkstm01@gmail.com"
+// var accountPassword="12345"
+// accountCity="Bihar"
+
+// //accountId=123
+
+// console.log(accountId);
+// console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// let score="123afdas"
+// console.log(typeof score);
+// let valueInNumber=Number(score) //string data type change in to number data type
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);// NaN means not a number
+
+let isloggedIn=0
+let booleanvalue=Boolean(isloggedIn)
+console.log(booleanvalue);
