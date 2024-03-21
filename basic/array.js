88ded46fd2272ce672c7abb167b6bbf0b987301e
rajivranjanyadav <160840661+rajivranjanyadav@users.noreply.github.com>
2024-03-21 09:57:38 +0000

    array done

---
+++ basic/array.js
@@ -0,0 +1,38 @@
+// const myarr=[1,2,3,4,5,6,7,8,9];
+//console.log(myarr);
+//const myhero=["papa", "Bhai"];
+//myhero.push("Family")
+
+//console.log(myhero);
+//myhero.pop()
+//console.log(myhero);
+
+// myarr.unshift(100);
+// myarr.shift()
+// console.log(myarr);
+
+// const newmyarr= myarr.join();
+// console.log(myarr);
+// console.log(typeof newmyarr);
+
+// console.log("A ", myarr);
+
+// const myn1 = myarr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myarr);
+
+
+// const myn2 = myarr.splice(1, 3)
+// console.log("C ", myarr);
+// console.log(myn2);
+
+const arr=["Rakesh", "Mukesh", "sujit", "Ranjesh"]
+
+const arr2=arr.slice(1,3)
+arr.splice(2,0,"Rajiv","Sanjeev")
+
+
+
+console.log(arr);
+console.log(arr2);
