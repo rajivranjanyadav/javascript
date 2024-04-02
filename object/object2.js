748d20604f243c79da112fc8a636c990ddd03662
rajivranjanyadav <160840661+rajivranjanyadav@users.noreply.github.com>
2024-04-02 11:53:40 +0000

    object done

---
+++ object/object2.js
@@ -0,0 +1,18 @@
+//const tender= new Object()
+const tender={}
+tender.name="Rajiv Ranjan Yadav"
+tender.age=23
+tender.city="Sitamarhi"
+tender.State="Bihar"
+//console.log(tender);
+
+const regularuser={
+    Email:"Rajiv@gmail.com",
+    fullName:{
+        userName:{
+            firstName:"Rajiv",
+            lastName:"Yadav"
+        }
+    }
+}
+console.log(regularuser.fullName.userName.firstName);
