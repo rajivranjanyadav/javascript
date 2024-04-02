a66f2db9eac8e78c9fad3e867694abfdc501c7a4
rajivranjanyadav <160840661+rajivranjanyadav@users.noreply.github.com>
2024-04-02 11:32:09 +0000

    object done

--- basic/objects.js
+++ basic/objects.js
@@ -23,17 +23,43 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+// JsUser.email = "hitesh@chatgpt.com"
+// // Object.freeze(JsUser)
+// JsUser.email = "hitesh@microsoft.com"
+// // console.log(JsUser);
 
-JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
+
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
+
+const my=Symbol("key1")
+const rks={
+    name:"Rajiv Ranjan Yadav",
+    age:23,
+    location:"koat bazar sitamrhi",
+    email:"rkstmo1@gmail.com",
+    [my]:"Nisha"
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+//console.log(rks);
+//console.log(typeof rks[my]);
+
+// rks.email="rajivranjankdh@gmail.com"
+// Object.freeze(rks)
+// rks.email="rajivranjankdhmicro@gmail.com"
+// console.log(rks);
+
+rks.gretting=function(){
+    console.log("Hello User");
+}
+rks.grettingTwo=function(){
+    console.log(`Hello User, ${this.name}`);
+}
+console.log(rks.gretting());
+console.log(rks.grettingTwo());
